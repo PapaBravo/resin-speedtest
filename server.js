9f91c4a97f2c0f1cd09e321c95ede4e3a5d76868
Paul Boeck <boeck87@gmail.com>
2018-07-01 11:47:06 +0200

    Add simple speedtest

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var speedTest = require('speedtest-net');
 
 // reply to request with "Hello World!"
 app.get('/', function (req, res) {
@@ -11,5 +12,15 @@ var server = app.listen(80, function () {
 
   var port = server.address().port;
   console.log('Example app listening on port ', port);
+
+  var test = speedTest({ maxTime: 5000 });
+
+  test.on('data', data => {
+    console.dir(data);
+  });
+
+  test.on('error', err => {
+    console.error(err);
+  });
 
 });
