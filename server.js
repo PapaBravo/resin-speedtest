e462e0bc54b2409a2e7da1aebfec9545c7946714
Paul Boeck <paul.boeck@cgi.com>
2018-07-05 08:30:32 +0200

    Add config

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
+
+const config = require(`./config/environment.${process.env.NODE_ENV || 'resin'}.json`);
+
 var express = require('express');
 var app = express();
 var speedTest = require('speedtest-net');
 const LocalStorage = require('node-localstorage').LocalStorage;
-const localStorage = new LocalStorage('/data/local-storage');
+const localStorage = new LocalStorage(config.storage);
 const storageKey = 'TIMES';
 
 const INTERVAL = 2 * 60 * 60 * 1000;
+
 
 // reply to request with "Hello World!"
 app.get('/', function (req, res) {
@@ -32,7 +36,7 @@ function testSpeed() {
 }
 
 //start a server on port 80 and log its start to our console
-var server = app.listen(80, function () {
+var server = app.listen(config.port, function () {
 
   var port = server.address().port;
   console.log('Timing app listening on port ', port);
